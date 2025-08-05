e311f71ee7810210e2484e3147a63fdd6f0c17c4
vaghelasanjay <vaghelasanjay1432@gmail.com>
2025-08-05 20:17:46 +0530

    first commit~

---
+++ src/app/comments/data.ts
@@ -0,0 +1,11 @@
+export const comments = [
+    {id:1,
+        text:"This is the First Comment"
+    },
+    {id:2,
+        text:"This is the Second Comment"
+    },
+    {id:3,
+        text:"This is the Third Comment"
+    }
+]
